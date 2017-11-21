f14b9d95d1c5647b248164ca1a18d3abf7233490
Glendon Solsberry <gms8994@gmail.com>
2017-11-21 13:21:23 -0500

    Moves stuff out of individual js files to a single one for now

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import _ from 'lodash';
+import Vue from 'vue'
+import axios from 'axios';
+
+require('moment');
+require('javascript-natural-sort');
+
+window.onload = function () {
+    new Vue({
+        el: '#app',
+        data: {
+            hosts: [],
+            tasks: [],
+            sortType: 'project',
+            sortReverse: 1,
+        },
+        ready: function() {
+            this.fetchHosts();
+        },
+        methods: {
+            fetchEvents: function() {
+                axios.get('/tasks.json').then((response) => {
+                    this.$set('tasks', response.body);
+                    setTimeout(this.fetchHosts, 5000);
+                }, (response) => {
+                    console.debug(response);
+                });
+            },
+            fetchHosts: function() {
+                axios.get('/hosts.json').then((response) => {
+                    this.$set('hosts', response.body);
+                    this.fetchEvents();
+                }, (response) => {
+                    console.debug(response);
+                });
+            },
+            moment: function () {
+                return moment();
+            }
+        },
+        filters: {
+            truncate: function(string, value) {
+                var len = string.length;
+                return string.substring(0, value) + (len > value ? '...' : '');
+            },
+            duration: function(number, format) {
+                return moment.duration(number * 1000).format(format, { trim: false });
+            },
+            status: function(status) {
+                return status == 1 ? 'Running' : 'Stopped';
+            },
+        caseInsensitiveOrderBy: function (arr, sortKey, reverse) {
+            // arr = convertArray(arr)
+            if (!sortKey) {
+                return arr
+            }
+            var order = (reverse && reverse < 0) ? -1 : 1
+            // sort on a copy to avoid mutating original array
+            return arr.slice().sort(function (a, b) {
+                if (sortKey !== '$key') {
+                    if (Vue.util.isObject(a) && '$value' in a) a = a.$value
+                    if (Vue.util.isObject(b) && '$value' in b) b = b.$value
+                }
+                a = Vue.util.isObject(a) ? Vue.parsers.path.getPath(a, sortKey) : a;
+                b = Vue.util.isObject(b) ? Vue.parsers.path.getPath(b, sortKey) : b;
+
+                var sorted = [ a, b ].sort(naturalSort);
+
+                return a === sorted[0] ? 0 : a > sorted[0] ? order : -order
+            });
+        }
+        }
+    });
+};
