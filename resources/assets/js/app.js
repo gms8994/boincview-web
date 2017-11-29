2580e8d280f00585fcab6aba0cbc786f9ec3bc49
Glendon Solsberry <gms8994@gmail.com>
2017-11-29 11:12:09 -0500

    axios uses response.data instead

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import axios from 'axios';
 
 require('moment');
-require('javascript-natural-sort');
+require('natural-sort');
 
 window.onload = function () {
     new Vue({
@@ -20,7 +20,7 @@ window.onload = function () {
         methods: {
             fetchEvents: function() {
                 axios.get('/tasks.json').then((response) => {
-                    this.$set('tasks', response.body);
+                    this.$set('tasks', response.data);
                     setTimeout(this.fetchHosts, 5000);
                 }, (response) => {
                     console.debug(response);
@@ -28,7 +28,7 @@ window.onload = function () {
             },
             fetchHosts: function() {
                 axios.get('/hosts.json').then((response) => {
-                    this.$set('hosts', response.body);
+                    this.$set('hosts', response.data);
                     this.fetchEvents();
                 }, (response) => {
                     console.debug(response);
@@ -49,26 +49,27 @@ window.onload = function () {
             status: function(status) {
                 return status == 1 ? 'Running' : 'Stopped';
             },
-        caseInsensitiveOrderBy: function (arr, sortKey, reverse) {
-            // arr = convertArray(arr)
-            if (!sortKey) {
-                return arr
-            }
-            var order = (reverse && reverse < 0) ? -1 : 1
-            // sort on a copy to avoid mutating original array
-            return arr.slice().sort(function (a, b) {
-                if (sortKey !== '$key') {
-                    if (Vue.util.isObject(a) && '$value' in a) a = a.$value
-                    if (Vue.util.isObject(b) && '$value' in b) b = b.$value
+            caseInsensitiveOrderBy: function (arr, sortKey, reverse) {
+                // arr = convertArray(arr)
+                if (!sortKey) {
+                    return arr
                 }
-                a = Vue.util.isObject(a) ? Vue.parsers.path.getPath(a, sortKey) : a;
-                b = Vue.util.isObject(b) ? Vue.parsers.path.getPath(b, sortKey) : b;
+                var order = (reverse && reverse < 0) ? -1 : 1
 
-                var sorted = [ a, b ].sort(naturalSort);
+                // sort on a copy to avoid mutating original array
+                return arr.slice().sort(function (a, b) {
+                    if (sortKey !== '$key') {
+                        if (Vue.util.isObject(a) && '$value' in a) a = a.$value
+                        if (Vue.util.isObject(b) && '$value' in b) b = b.$value
+                    }
+                    a = Vue.util.isObject(a) ? Vue.parsers.path.getPath(a, sortKey) : a;
+                    b = Vue.util.isObject(b) ? Vue.parsers.path.getPath(b, sortKey) : b;
 
-                return a === sorted[0] ? 0 : a > sorted[0] ? order : -order
-            });
-        }
+                    var sorted = [ a, b ].sort(naturalSort);
+
+                    return a === sorted[0] ? 0 : a > sorted[0] ? order : -order
+                });
+            }
         }
     });
 };
